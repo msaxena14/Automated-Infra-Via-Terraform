fdd460554850dac7d10b70ec29a36643cb78a53c
Madhavam <madhavam.saxena@paytm.com>
2024-02-24 23:45:04 +0530

    checking

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,31 +9,3 @@ resource "aws_vpc" "main" {
     Name = "${var.vpc_name}-vpc"
   }
 }
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.vpc_name}-igw"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = ""
-  availability_zone = "ap-south-1a"
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = ""
-    gateway_id = aws_internet_gateway.main.id
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
