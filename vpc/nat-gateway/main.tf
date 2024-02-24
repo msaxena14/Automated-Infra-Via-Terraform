b09d89c188f77b0fd126e94863990397face5c5b
Madhavam <madhavam.saxena@paytm.com>
2024-02-25 02:02:04 +0530

    checking

--- vpc/nat-gateway/main.tf
+++ vpc/nat-gateway/main.tf
@@ -1,6 +1,6 @@
 resource "aws_nat_gateway" "nat" {
   allocation_id = var.eip_id
-  subnet_id     = var.private-subnet
+  subnet_id     = var.private-subnet[0]
 
   tags = {
     Name = "${var.project}-${var.environment}-nat-gw"
