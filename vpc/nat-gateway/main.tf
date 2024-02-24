54ec000110e272b924235fea53f644f9b693b691
Madhavam <madhavam.saxena@paytm.com>
2024-02-25 01:58:08 +0530

    checking

--- vpc/nat-gateway/main.tf
+++ vpc/nat-gateway/main.tf
@@ -8,5 +8,5 @@ resource "aws_nat_gateway" "nat" {
 
   # To ensure proper ordering, it is recommended to add an explicit dependency
   # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.example]
+  depends_on = [aws_internet_gateway.nat]
 }
