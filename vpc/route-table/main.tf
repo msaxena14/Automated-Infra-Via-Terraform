668b1e9f3988fc4b12e9ec3da6c8b4a1a97e12a7
Madhavam <madhavam.saxena@paytm.com>
2024-02-25 02:32:00 +0530

    checking

--- vpc/route-table/main.tf
+++ vpc/route-table/main.tf
@@ -3,7 +3,7 @@ resource "aws_route_table" "second_rt" {
  
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
+        gateway_id = var.igw.id
     }
  
     tags = {
