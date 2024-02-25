c4de6b9c91dfc2b33173be9411ed07a138bc70d9
Madhavam <madhavam.saxena@paytm.com>
2024-02-25 16:14:38 +0530

    Fixed

--- vpc/main.tf
+++ vpc/main.tf
@@ -47,10 +47,12 @@ module "nat-gateway" {
 # calling route-table module
 module "route-table" {
   source            = "./route-table"
-  count             = length(module.public-subnet.subnet_id[*])
+  # count             = length(module.public-subnet.subnet_id[*])
   igw_id            = "${module.internet-gateway.igw_id}"
   nat_gateway_id    = "${module.nat-gateway.nat_gateway_id}"
   vpc_id            = "${aws_vpc.main.id}"
-  public_subnet_id  = element(module.public-subnet.subnet_id[*], count.index)
-  private_subnet_id = element(module.private-subnet.subnet_id[*], count.index)
+  public_subnet_id  = "${module.public-subnet.subnet_id[*]}"
+  private_subnet_id = "${module.private-subnet.subnet_id[*]}"
+  # public_subnet_id  = element(module.public-subnet.subnet_id[*], count.index)
+  # private_subnet_id = element(module.private-subnet.subnet_id[*], count.index)
 }
